7cfaab8bfb57a450d70c7ab41d6be68d2b87cf24
Snehith Challa <challasnehith@gmail.com>
2018-12-30 20:18:07 -0500

    Create destructor.js
    How destructor works

---
+++ destructor.js
@@ -0,0 +1,19 @@
+var john = ['john', 26];
+
+const [name, cool] = ['john', 26];
+
+console.log(`${name} ${cool}`);
+
+
+function myFunc(){
+	return [1,2,3];
+}
+
+var o = {
+	p:55,
+	q: true
+};
+
+var {p,q} = o;
+
+console.log(`${p}  ${q}`);
